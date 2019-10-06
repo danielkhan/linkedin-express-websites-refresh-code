5a4a3edd3fb645ebd1f4a0c13afc407ce9fcb1dc
danielkhan <daniel@khan.io>
2019-10-06 15:47:10 +0200

    progress

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello Express :)'));
+app.use(express.static(path.join(__dirname, './static')));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/index.html'));
+});
+
+app.get('/speakers', (req, res) => {
+  res.sendFile(path.join(__dirname, './static/speakers.html'));
+});
 
 app.listen(port, () => console.log(`Express server listening on port ${port}!`));
