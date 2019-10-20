0cc93f7972635041c2372447835083c8322e0e31
danielkhan <daniel@khan.io>
2019-10-20 16:18:56 +0200

    updates

--- provided/services/SpeakerService.js
+++ provided/services/SpeakerService.js
@@ -1,24 +1,46 @@
 const fs = require("fs");
 const util = require("util");
 
+/**
+ * We want to use async/await with fs.readFile - util.promisfy gives us that
+ */
 const readFile = util.promisify(fs.readFile);
 
+/**
+ * Logic for fetching speakers information
+ */
 class SpeakerService {
+  /**
+   * Constructor
+   * @param {*} datafile Path to a JSOn file that contains the speakers data
+   */
   constructor(datafile) {
     this.datafile = datafile;
   }
 
+  /**
+   * Returns a list of speakers name and short name
+   */
   async getNames() {
     const data = await this.getData();
+
+    // We are using map() to transform the array we get into another one
     return data.map(speaker => {
       return { name: speaker.name, shortname: speaker.shortname };
     });
   }
 
+  /**
+   * Get all artwork
+   */
   async getAllArtwork() {
     const data = await this.getData();
+
+    // Array.reduce() is used to traverse all speakers and
+    // create an array that contains all artwork
     const artwork = data.reduce((acc, elm) => {
       if (elm.artwork) {
+        // eslint-disable-next-line no-param-reassign
         acc = [...acc, ...elm.artwork];
       }
       return acc;
@@ -26,19 +48,27 @@ class SpeakerService {
     return artwork;
   }
 
+  /**
+   * Get all artwork of a given speaker
+   * @param {*} shortname The speakers short name
+   */
   async getArtworkForSpeaker(shortname) {
     const data = await this.getData();
-    const speaker = data.find(speaker => {
-      return speaker.shortname === shortname;
+    const speaker = data.find(elm => {
+      return elm.shortname === shortname;
     });
     if (!speaker || !speaker.artwork) return null;
     return speaker.artwork;
   }
 
+  /**
+   * Get speaker information provided a shortname
+   * @param {*} shortname
+   */
   async getSpeaker(shortname) {
     const data = await this.getData();
-    const speaker = data.find(speaker => {
-      return speaker.shortname === shortname;
+    const speaker = data.find(elm => {
+      return elm.shortname === shortname;
     });
     if (!speaker) return null;
     return {
@@ -49,6 +79,9 @@ class SpeakerService {
     };
   }
 
+  /**
+   * Returns a list of speakers with only the basic information
+   */
   async getListShort() {
     const data = await this.getData();
     return data.map(speaker => {
@@ -60,6 +93,9 @@ class SpeakerService {
     });
   }
 
+  /**
+   * Get a list of speakers
+   */
   async getList() {
     const data = await this.getData();
     return data.map(speaker => {
@@ -72,6 +108,9 @@ class SpeakerService {
     });
   }
 
+  /**
+   * Fetches speakers data from the JSON file provided to the constructor
+   */
   async getData() {
     const data = await readFile(this.datafile, "utf8");
     return JSON.parse(data).speakers;
