0c751cfe175645c88145b8a61f492c493c870a1c
danielkhan <daniel@khan.io>
2019-10-20 16:39:48 +0200

    progress

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
-const routes = require("./routes");
+const FeedbackService = require('./services/FeedbackService');
+const SpeakerService = require('./services/SpeakerService');
+
+const feedbackService = new FeedbackService('./data/feedback.json');
+const speakerService = new SpeakerService('./data/speakers.json');
+
+const routes = require('./routes');
 
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, "./static")));
+app.use(express.static(path.join(__dirname, './static')));
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.use(routes());
-
-app.listen(port, () =>
-  console.log(`Express server listening on port ${port}!`)
+app.use(
+  routes({
+    feedbackService,
+    speakerService,
+  })
 );
+
+app.listen(port, () => console.log(`Express server listening on port ${port}!`));
