899fec3f97f6a829702a37de3752260c55130813
danielkhan <daniel@khan.io>
2019-11-03 21:43:41 -0800

    add feedback items

--- routes/feedback.js
+++ routes/feedback.js
@@ -6,6 +6,31 @@ const router = express.Router();
 module.exports = params => {
   const { feedbackService } = params;
 
+  const validations = [
+    check('name')
+      .trim()
+      .isLength({ min: 3 })
+      .escape()
+      .withMessage('A name required')
+      .escape(),
+    check('email')
+      .trim()
+      .isEmail()
+      .normalizeEmail()
+      .withMessage('A valid email address required')
+      .escape(),
+    check('title')
+      .trim()
+      .isLength({ min: 3 })
+      .withMessage('A title is required')
+      .escape(),
+    check('message')
+      .trim()
+      .isLength({ min: 5 })
+      .withMessage('A message is required')
+      .escape(),
+  ];
+
   router.get('/', async (request, response) => {
     const feedback = await feedbackService.getList();
     const errors = request.session.feedback ? request.session.feedback.errors : false;
@@ -21,53 +46,48 @@ module.exports = params => {
     });
   });
 
-  router.post(
-    '/',
-    [
-      check('name')
-        .trim()
-        .isLength({ min: 3 })
-        .escape()
-        .withMessage('A name required')
-        .escape(),
-      check('email')
-        .trim()
-        .isEmail()
-        .normalizeEmail()
-        .withMessage('A valid email address required')
-        .escape(),
-      check('title')
-        .trim()
-        .isLength({ min: 3 })
-        .withMessage('A title is required')
-        .escape(),
-      check('message')
-        .trim()
-        .isLength({ min: 5 })
-        .withMessage('A message is required')
-        .escape(),
-    ],
-    async (request, response, next) => {
-      try {
-        const errors = validationResult(request);
+  router.post('/', validations, async (request, response, next) => {
+    try {
+      const errors = validationResult(request);
 
-        if (!errors.isEmpty()) {
-          request.session.feedback = {
-            errors: errors.array(),
-          };
-          return response.redirect('/feedback');
-        }
-
-        const { name, email, title, message } = request.body;
-        await feedbackService.addEntry(name, email, title, message);
+      if (!errors.isEmpty()) {
         request.session.feedback = {
-          message: 'Thank you for your feedback!',
+          errors: errors.array(),
         };
         return response.redirect('/feedback');
-      } catch (err) {
-        return next(err);
       }
+
+      const { name, email, title, message } = request.body;
+      await feedbackService.addEntry(name, email, title, message);
+      request.session.feedback = {
+        message: 'Thank you for your feedback!',
+      };
+      return response.redirect('/feedback');
+    } catch (err) {
+      return next(err);
     }
-  );
+  });
+
+  router.post('/api', validations, async (request, response) => {
+    // let's check the validationresult
+    const errors = validationResult(request);
+
+    // if there are errors ..
+    if (!errors.isEmpty()) {
+      // We will just return a JSON object containing all errors using response.json
+      return response.json({ errors: errors.array() });
+    }
+
+    const { name, email, title, message } = request.body;
+    // if there were no errors, we will store the data as we did with the form as well
+    await feedbackService.addEntry(name, email, title, message);
+
+    // so we fetch the data
+    const feedback = await feedbackService.getList();
+
+    // and return it
+    return response.json({ feedback });
+  });
+
   return router;
 };
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const routes = require('./routes');
 const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     name: 'session',
